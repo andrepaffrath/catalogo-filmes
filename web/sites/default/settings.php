b26fd8a3370194249ebf9624d0dcc1f914a7e708
Andre Paffrath Batista <apaffrat@emeal.nttdata.com>
2022-03-15 16:59:04 -0300

    Inclusão css do menu, organização do bloco header e menu

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -780,3 +780,7 @@ $databases['default']['default'] = array (
   'driver' => 'mysql',
 );
 $settings['config_sync_directory'] = '../config/sync';
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+   include $app_root . '/' . $site_path . '/settings.local.php';
+}
---
+++ web/themes/custom/CF/js/start.js
@@ -0,0 +1,8 @@
+(function ($){
+
+    $(document).ready(function()
+        {
+            $('ul.menu--main li:has(a.is-active)').addClass('is-active');
+            
+        })
+})(jQuery);
